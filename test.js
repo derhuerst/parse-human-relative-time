d8f9afd62318066139ca88b958349c50b25a54e3
Jannis R <mail@jannisr.de>
2019-11-15 14:44:18 +0100

    test for Luxon integration :white_check_mark:

--- test.js
+++ test.js
@@ -2,178 +2,206 @@
 
 const {deepStrictEqual: eql} = require('assert')
 const {toDate} = require('date-fns-tz')
+const {DateTime} = require('luxon')
 const lex = require('./lex')
 const parse = require('.')
+const parseWithLuxon = require('./luxon')
 
 const resetHours = ['setHours', 0]
 const resetMinutes = ['setMinutes', 0]
 const resetSeconds = ['setSeconds', 0]
 const resetMilliseconds = ['setMilliseconds', 0]
 
-eql(lex('12am'), [
-	['setHours', 0],
-	resetMinutes,
-	resetSeconds,
-	resetMilliseconds
-])
-eql(lex('12:01am'), [
-	['setHours', 0],
-	['setMinutes', 1],
-	resetSeconds,
-	resetMilliseconds
-])
-eql(lex('1am'), [
-	['setHours', 1],
-	resetMinutes,
-	resetSeconds,
-	resetMilliseconds
-])
-eql(lex('11:59am'), [
-	['setHours', 11],
-	['setMinutes', 59],
-	resetSeconds,
-	resetMilliseconds
-])
+const timeZone = 'Europe/Berlin'
+const now = '2019-03-31T01:59:00+01:00'
 
-eql(lex('noon'), [
-	['setHours', 12],
-	resetMinutes,
-	resetSeconds,
-	resetMilliseconds
-])
-eql(lex('12pm'), [
-	['setHours', 12],
-	resetMinutes,
-	resetSeconds,
-	resetMilliseconds
-])
+const tests = [
+	['12am', [
+		['setHours', 0],
+		resetMinutes,
+		resetSeconds,
+		resetMilliseconds
+	], '2019-03-31T00:00:00+01:00'],
+	['12:01am', [
+		['setHours', 0],
+		['setMinutes', 1],
+		resetSeconds,
+		resetMilliseconds
+	], '2019-03-31T00:01:00+01:00'],
+	['1am', [
+		['setHours', 1],
+		resetMinutes,
+		resetSeconds,
+		resetMilliseconds
+	], '2019-03-31T01:00:00+01:00'],
+	['11:59am', [
+		['setHours', 11],
+		['setMinutes', 59],
+		resetSeconds,
+		resetMilliseconds
+	], '2019-03-31T11:59:00+02:00'],
 
-eql(lex('12:01pm'), [
-	['setHours', 12],
-	['setMinutes', 1],
-	resetSeconds,
-	resetMilliseconds
-])
-eql(lex('11:59pm'), [
-	['setHours', 23],
-	['setMinutes', 59],
-	resetSeconds,
-	resetMilliseconds
-])
+	['noon', [
+		['setHours', 12],
+		resetMinutes,
+		resetSeconds,
+		resetMilliseconds
+	], '2019-03-31T12:00:00+02:00'],
+	['12pm', [
+		['setHours', 12],
+		resetMinutes,
+		resetSeconds,
+		resetMilliseconds
+	], '2019-03-31T12:00:00+02:00'],
 
-eql(lex('midnight'), [
-	['setHours', 0],
-	resetMinutes,
-	resetSeconds,
-	resetMilliseconds
-])
+	['12:01pm', [
+		['setHours', 12],
+		['setMinutes', 1],
+		resetSeconds,
+		resetMilliseconds
+	], '2019-03-31T12:01:00+02:00'],
+	['11:59pm', [
+		['setHours', 23],
+		['setMinutes', 59],
+		resetSeconds,
+		resetMilliseconds
+	], '2019-03-31T23:59:00+02:00'],
 
-eql(lex('tomorrow at 7'), [
-	['addDays', 1],
-	['setHours', 7],
-	resetMinutes,
-	resetSeconds,
-	resetMilliseconds
-])
-eql(lex('yesterday at 7'), [
-	['subDays', 1],
-	['setHours', 7],
-	resetMinutes,
-	resetSeconds,
-	resetMilliseconds
-])
-eql(lex('today at 8am'), [
-	['setHours', 8],
-	resetMinutes,
-	resetSeconds,
-	resetMilliseconds
-])
-eql(lex('now'), [])
-eql(lex('3 days ago'), [
-	['subDays', 3]
-])
-eql(lex('a week ago'), [
-	['subWeeks', 1]
-])
-eql(lex('2 weeks ago'), [
-	['subWeeks', 2]
-])
+	['midnight', [
+		['setHours', 0],
+		resetMinutes,
+		resetSeconds,
+		resetMilliseconds
+	], '2019-03-31T00:00:00+01:00'],
 
-// todo: `aug 25 2015 5pm`
-eql(lex('aug 25th 2015 5pm'), [
-	['setDate', 25],
-	['setMonth', 7], // 7 = August
-	['setYear', 2015],
-	['setHours', 17],
-	resetMinutes,
-	resetSeconds,
-	resetMilliseconds
-])
-eql(lex('august 25th 2015'), [
-	['setDate', 25],
-	['setMonth', 7], // 7 = August
-	['setYear', 2015]
-])
-eql(lex('1st of Jan 2020'), [
-	['setDate', 1],
-	['setMonth', 0], // 0 = January
-	['setYear', 2020]
-])
-eql(lex('30th of August'), [
-	['setDate', 30],
-	['setMonth', 7] // 7 = August
-])
+	['tomorrow at 7', [
+		['addDays', 1],
+		['setHours', 7],
+		resetMinutes,
+		resetSeconds,
+		resetMilliseconds
+	], '2019-04-01T07:00:00+02:00'],
+	['yesterday at 7', [
+		['subDays', 1],
+		['setHours', 7],
+		resetMinutes,
+		resetSeconds,
+		resetMilliseconds
+	], '2019-03-30T07:00:00+01:00'],
+	['today at 8am', [
+		['setHours', 8],
+		resetMinutes,
+		resetSeconds,
+		resetMilliseconds
+	], '2019-03-31T08:00:00+02:00'],
+	['now', [], now],
+	['3 days ago', [
+		['subDays', 3]
+	], '2019-03-28T01:59:00+01:00'],
+	['a week ago', [
+		['subWeeks', 1]
+	], '2019-03-24T01:59:00+01:00'],
+	['2 weeks ago', [
+		['subWeeks', 2]
+	], '2019-03-17T01:59:00+01:00'],
 
-eql(lex('sunday'), [
-	['setDay', 0] // 0 = Sunday
-])
-eql(lex('this friday'), [
-	['setDay', 5] // 5 = Friday
-])
-eql(lex('next friday'), [
-	['startOfWeek'],
-	['addWeeks', 1],
-	['setDay', 5] // 5 = Friday
-])
-eql(lex('last friday'), [
-	['startOfWeek'],
-	['subWeeks', 1],
-	['setDay', 5] // 5 = Friday
-])
+	// todo: `aug 25 2015 5pm`
+	['aug 25th 2015 5pm', [
+		['setDate', 25],
+		['setMonth', 7], // 7 = August
+		['setYear', 2015],
+		['setHours', 17],
+		resetMinutes,
+		resetSeconds,
+		resetMilliseconds
+	], '2015-08-25T17:00:00+02:00'],
+	['august 25th 2015', [
+		['setDate', 25],
+		['setMonth', 7], // 7 = August
+		['setYear', 2015]
+	], '2015-08-25T01:59:00+02:00'],
+	['1st of Jan 2020', [
+		['setDate', 1],
+		['setMonth', 0], // 0 = January
+		['setYear', 2020]
+	], '2020-01-01T01:59:00+01:00'],
+	['30th of August', [
+		['setDate', 30],
+		['setMonth', 7] // 7 = August
+	], '2019-08-30T01:59:00+02:00'],
 
-eql(lex('in 12 minutes'), [
-	['addMinutes', 12]
-])
-eql(lex('in 2 hours'), [
-	['addHours', 2]
-])
-eql(lex('in 31 hours'), [
-	['addHours', 31]
-])
-eql(lex('in 20 hours 40 minutes'), [
-	['addHours', 20],
-	['addMinutes', 40]
-])
-eql(lex('in 20 hours and 40 minutes'), [
-	['addHours', 20],
-	['addMinutes', 40]
-])
-eql(lex('in 20.2h'), [
-	['addHours', 20.2]
-])
-eql(lex('in 5 weeks'), [
-	['addWeeks', 5]
-])
-eql(lex('in 2 years'), [
-	['addYears', 2]
-])
-eql(lex('in 2 years and 5 weeks'), [
-	['addYears', 2],
-	['addWeeks', 5]
-])
-eql(lex('in 1.5 weeks'), [
-	['addWeeks', 1.5]
-])
+	['friday', [
+		['setDay', 5] // 5 = Friday
+	], '2019-03-29T01:59:00+01:00'],
+	['this friday', [
+		['setDay', 5] // 5 = Friday
+	], '2019-03-29T01:59:00+01:00'],
+	// todo: should be 2019-04-05T01:59:00+02:00
+	['next friday', [
+		['startOfWeek'],
+		['addWeeks', 1],
+		['setDay', 5] // 5 = Friday
+	], '2019-04-05T00:00:00+02:00'],
+	// todo: should be 2019-03-22T01:59:00+02:00
+	['last friday', [
+		['startOfWeek'],
+		['subWeeks', 1],
+		['setDay', 5] // 5 = Friday
+	], '2019-03-22T00:00:00+01:00'],
+
+	['in 12 minutes', [
+		['addMinutes', 12]
+	], '2019-03-31T03:11:00+02:00'],
+	['in 2 hours', [
+		['addHours', 2]
+	], '2019-03-31T04:59:00+02:00'],
+	['in 31 hours', [
+		['addHours', 31]
+	], '2019-04-01T09:59:00+02:00'],
+	['in 20 hours 40 minutes', [
+		['addHours', 20],
+		['addMinutes', 40]
+	], '2019-03-31T23:39:00+02:00'],
+	['in 20 hours and 40 minutes', [
+		['addHours', 20],
+		['addMinutes', 40]
+	], '2019-03-31T23:39:00+02:00'],
+	['in 20.2h', [
+		['addHours', 20.2]
+	], '2019-03-31T23:11:00+02:00'],
+	// todo: should be 2019-05-05T02:59:00+02:00
+	['in 5 weeks', [
+		['addWeeks', 5]
+	], '2019-05-05T01:59:00+02:00'],
+	['in 2 years', [
+		['addYears', 2]
+	], '2021-03-31T01:59:00+02:00'],
+	['in 2 years and 5 weeks', [
+		['addYears', 2],
+		['addWeeks', 5]
+	], '2021-05-05T01:59:00+02:00'],
+	['in 1.5 weeks', [
+		['addWeeks', 1.5]
+	], '2019-04-10T13:59:00+02:00']
+]
+
+for (const [relative, instructions, iso] of tests) {
+	console.info(relative)
+
+	eql(lex(relative), instructions)
+
+	// todo: test `parse` once https://github.com/marnusw/date-fns-tz/issues/31 is fixed
+
+	const d = DateTime.fromISO(now).setZone('Europe/Berlin')
+	const d2 = parseWithLuxon(relative, d)
+	const actual = d2.toISO({suppressMilliseconds: true})
+	try {
+		eql(actual, iso)
+	} catch (err) {
+		err.relative = relative
+		throw err
+	}
+}
 
 
 // 01:00 in Europe/Berlin: switch non-DST -> DST
@@ -184,10 +212,9 @@ eql(lex('in 1.5 weeks'), [
 //   .plus({minutes: 2})
 //   .toISO()
 //   -> 2019-03-31T03:01+02:00
-const timeZone = 'Europe/Berlin'
 const d = toDate('2019-03-31T01:59+01:00', {timeZone})
 const d2 = parse('in 2 minutes', d)
 eql(d2.toISOString(), '2019-03-31T01:01:00.000Z')
 
 
-console.info('Tests successful. ✔︎')
+console.info('\nTests successful. ✔︎')
